4bfaaf83e4a00c28bde64ff557badbdf15089d13
JustALostSoul <135549816+JustALostSoul@users.noreply.github.com>
2023-12-12 09:43:28 +0000

    Implemented Markdown Changelog

---
+++ public/changelog.md
@@ -0,0 +1,22 @@
+# Website Changelog
+
+
+## Website Update 0.1.4 - 11.12.2023
+- Neues Dokument hochgeladen
+- Download Page etwas übersichtlicher gestaltet
+
+
+## Website Update 0.1.3 - 10.12.2023
+Automated Github Action eingerichtet, für schnellen und automatisches Deploying auf den Webspace
+
+
+## Website Update 0.1.2 - 10.12.2023
+Routing fixed mittels searchParametern und meta-Refresh-Weiterleitung von /de/hoga
+
+
+## Website Update 0.1.1 - 09.12.2023
+Komplettes Backend der Website überarbeitet. Nun bessere und schnellere Funktion und bessere Entwicklung möglich. Beim nächsten Update hoffentlich Versionskontrolle auf Github inkl. automatischem Deploy auf den Webspace
+
+
+## Website Update 0.1.0 - 08.12.2023
+Website ging online
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,39 +1,42 @@
-const messages = [
-    {
-        name: "Website Update 0.1.4 - 11.12.2023 19:55 Uhr",
-        content: "Neues Dokument hochgeladen, Download Page etwas übersichtlicher gestaltet"
-    },
-    {
-        name: "Website Update 0.1.3 - 10.12.2023 14:45 Uhr",
-        content: "Automated Github Action eingerichtet, für schnellen und automatisches Deploying auf den Webspace"
-    },
-    {
-        name: "Website Update 0.1.2 - 10.12.2023 13 Uhr",
-        content: "Routing fixed mittels searchParametern und meta-Refresh-Weiterleitung von /de/hoga"
-    },
-    {
-        name: "Website Update 0.1.1 - 09.12.2023 17 Uhr",
-        content: "Komplettes Backend der Website überarbeitet. Nun bessere und schnellere Funktion und bessere Entwicklung möglich. Beim nächsten Update hoffentlich Versionskontrolle auf Github inkl. automatischem Deploy auf den Webspace"
-    },
-    {
-        name: "Website Update 0.1.0 - 08.12.2023 22 Uhr",
-        content: "Website ging online"
-    },
-]
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Markdown from 'react-markdown';
+
+const markdown = '# Hi, *Pluto*!';
 
 export function Dashboard() {
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+        fetch("./changelog.md")
+            .then((res) => res.text())
+            .then((text) => setContent(text));
+    }, []);
+
     return(
         <div>
-            <ul className="divide-y divide-gray-100">
-                {messages.map((msg) => (
-                    <li key={msg.name}>
-                        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                            <h1 className="text-3xl font-bold tracking-tight text-gray-900">{msg.name}</h1> 
-                            <p>{msg.content}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <Markdown
+                className="markdown"
+                children={content}
+                components={{
+                    h1(props) {
+                        const {node, ...rest} = props
+                        return <h1 class='text-3xl font-bold tracking-tight text-gray-900' {...rest}/>
+                    },
+                    h2(props) {
+                        const {node, ...rest} = props
+                        return <h2 class='text-2xl font-bold tracking-tight text-gray-900' {...rest}/>
+                    },
+                    h3(props) {
+                        const {node, ...rest} = props
+                        return <h3 class='text-xl font-bold tracking-tight text-gray-900' {...rest}/>
+                    },
+                    ul(props){
+                        const{node, ...rest} = props
+                        return <ul class='list-disc' {...rest}/>
+                    }
+                }}
+            / >
         </div>
     )
 }
